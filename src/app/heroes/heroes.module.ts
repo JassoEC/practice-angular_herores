eedd23057730682715bbedbb43be4bfd00740a67
Emanuel Campos Jasso <isw.ecamposj@gmail.com>
2021-10-19 22:12:02 -0500

    se realizo la creacion de componentes varios

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddComponent } from './pages/add/add.component';
+import { HeroComponent } from './pages/hero/hero.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListComponent } from './pages/list/list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddComponent,
+    HeroComponent,
+    HomeComponent,
+    ListComponent
+  ],
   imports: [
     CommonModule
   ]
