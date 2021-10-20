0bc54aaf920affcdc2c1259a25a2d3700bdf8267
Emanuel Campos Jasso <isw.ecamposj@gmail.com>
2021-10-19 22:39:29 -0500

    se agrego el ejemplo de lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
   { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '404' },
 ];
