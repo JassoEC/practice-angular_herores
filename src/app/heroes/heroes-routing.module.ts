63a4f546d5733da96324374623eaaef7d335ecd3
Emanuel Campos Jasso <isw.ecamposj@gmail.com>
2021-10-20 22:05:01 -0500

    se agrego el uso de las rutas anidadas

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddComponent } from './pages/add/add.component';
 import { HeroComponent } from './pages/hero/hero.component';
+import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
 import { SearchComponent } from './pages/search/search.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: [
       { path: 'list', component: ListComponent },
       { path: 'add', component: AddComponent },
